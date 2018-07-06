93e4c3538437375816b81d6767ab355bce0291c9
William <34868604+yWilliamPL@users.noreply.github.com>
2018-07-06 13:30:37 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,14 +70,17 @@ async def on_message(message):
     if message.content.lower().startswith(prefix+'avatar'):
       user = message.author.name
       usuario = message.mentions[0]
-      avatar_embed = discord.Embed(
-        title="Avatar do(a): {}".format(usuario.name),
-        color=0xFFFFFF,
-        description="[Clique aqui]("+usuario.avatar_url+") para ver o avatar!"
-      )
-      avatar_embed.set_image(url=usuario.avatar_url)
-      avatar_embed.set_footer(text="• Comando enviado por: {}.".format(user))
-      await client.send_message(message.channel, embed=avatar_embed)
+      try:
+        avatar_embed = discord.Embed(
+          title="Avatar do(a): {}".format(usuario.name),
+          color=0xFFFFFF,
+          description="[Clique aqui]("+usuario.avatar_url+") para ver o avatar!"
+        )
+        avatar_embed.set_image(url=usuario.avatar_url)
+        avatar_embed.set_footer(text="• Comando enviado por: {}.".format(user))
+        await client.send_message(message.channel, embed=avatar_embed)
+      finally:
+        pass
 
 
 
