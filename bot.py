9846489094c3f140a169d79a99c2bc588755dbbe
William <34868604+yWilliamPL@users.noreply.github.com>
2018-07-05 23:49:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,14 +6,43 @@ prefix = "/"
 
 @client.event
 async def on_ready():
-  await client.change_presence(game=discord.Game(name=prefix+"skell", type=2))
+  await client.change_presence(game=discord.Game(name=prefix+"comandos", type=2))
   print('[BOT ONLINE COM SUCESSO]')
 
 
 @client.event
 async def on_message(message):
-  if message.content.lower().startswith(prefix+'skell'):
-    await client.send_message(message.channel, "{},\n```fix\nSem palavras para descrever o ySkell!!!\n```".format(message.author.mention))
+  if message.content.lower().startswith(prefix+'comandos'):
+    await client.send_message(message.channel, "{},\n```diff\n- Este comando não está disponível no momento.\n```".format(message.author.mention))
+
+    
+  if message.content.lower().startswith(prefix+'ping'):
+    timep = time.time()
+    emb = discord.Embed(title = 'Aguarde...', color = COR)
+    pingm0 = await client.send_message(message.channel, embed=emb)
+    ping = time.time() - timep
+    pingm1 = discord.Embed(title = 'Pong!', description = '🏓 Ping - %.01f segundos' % ping, color=COR)
+    await client.edit_message(pingm0, embed=pingm1)
+
+
+  if message.content.startswith(prefix+'info'):
+    user = message.author.name
+    horario = datetime.datetime.now().strftime("%H:%M:%S")
+    serverinfo_embed = discord.Embed(title="\n", description="Abaixo está as informaçoes principais do servidor!", color=COR)
+    serverinfo_embed.set_thumbnail(url=message.server.icon_url)
+    serverinfo_embed.set_footer(text="{} • {}".format(user, horario))
+    serverinfo_embed.add_field(name="Nome:", value=message.server.name, inline=True)
+    serverinfo_embed.add_field(name="Dono:", value=message.server.owner.mention)
+    serverinfo_embed.add_field(name="ID:", value=message.server.id, inline=True)
+    serverinfo_embed.add_field(name="Cargos:", value=len(message.server.roles), inline=True)
+    serverinfo_embed.add_field(name="Canais de texto:", value=len([message.channel.mention for channel in message.server.channels if channel.type==discord.ChannelType.text]), inline=True)
+    serverinfo_embed.add_field(name="Canais de voz:", value=len([message.channel.mention for channel in message.server.channels if channel.type==discord.ChannelType.voice]), inline=True)
+    serverinfo_embed.add_field(name="Membros:", value=len(message.server.members), inline=True)
+    serverinfo_embed.add_field(name="Bots:", value=len([user.mention for user in message.server.members if user.bot]), inline=True)        
+    serverinfo_embed.add_field(name="Criado em:", value=message.server.created_at.strftime("%d %b %Y %H:%M"), inline=True)
+    serverinfo_embed.add_field(name="Região:", value=str(message.server.region).title(), inline=True)
+    await client.send_message(message.channel,embed=serverinfo_embed)
+
 
 
 client.run('NDY0NjA0NDczOTMxODU3OTIx.DiBYJw.S2iTn7TXy7L9D1r1nLqryoaNOwg')
