48b0ea9fd67c406e5095393521f20995a0fd5f99
William <34868604+yWilliamPL@users.noreply.github.com>
2018-07-06 00:00:58 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,12 +18,12 @@ async def on_message(message):
 
 
   if message.content.lower().startswith(prefix+'ping'):
-    timep = time.time()
-    emb = discord.Embed(title = 'Aguarde...', color = COR)
-    pingm0 = await client.send_message(message.channel, embed=emb)
-    ping = time.time() - timep
-    pingm1 = discord.Embed(title = 'Pong!', description = '🏓 Ping - %.01f segundos' % ping, color=COR)
-    await client.edit_message(pingm0, embed=pingm1)
+        timep = time.time()
+        emb = discord.Embed(title = 'Aguarde...', color = COR)
+        pingm0 = await client.send_message(message.channel, embed=emb)
+        ping = time.time() - timep
+        pingm1 = discord.Embed(title = 'Pong!', description = '🏓 Ping - %.01f segundos' % ping, color=COR)
+        await client.edit_message(pingm0, embed=pingm1)
 
 
   if message.content.startswith(prefix+'info'):
