806dc54d133d4d32317555b1f5aaac70c2757af9
William <34868604+yWilliamPL@users.noreply.github.com>
2018-07-08 20:24:28 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,12 +155,12 @@ async def on_message(message):
       user = message.mentions[0].strip("<>@")
       cargo = str(message.content).strip(" ")
       cargo2 = cargo.split()
-      print(cargo)
+      print(user)
       role = discord.utils.find(lambda r: r.name == cargo[1], message.server.roles)
       canal = client.get_channel("465637883500298240")
       cargo2_embed = discord.Embed(title="O usuário **{}** foi adicionado ao cargo **{}**!".format(user, cargo), color=0xFF0000)
       cargo2_embed.set_footer(text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
-      await client.add_roles(user, role)
+      await client.add_roles(user.id, role)
       await client.send_message(canal, embed=cargo2_embed)
     finally:
       pass
