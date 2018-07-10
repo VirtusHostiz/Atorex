949f76ce0e990437d9ebca1c1fc91fdc68a6d9e8
yWilliamPDL <34868604+yWilliamPDL@users.noreply.github.com>
2018-07-09 23:23:42 -0300

    Update

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ async def on_message(message):
             unban_embed02.set_footer(text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
             await client.send_message(message.channel, embed=unban_embed02)
             unban_embed03 = discord.Embed(title="Usuário desbanido!", color=0x00FF00)
-            ban_embed03.add_field(name="Usuário:", value=user)
+            unban_embed03.add_field(name="Usuário:", value=user)
             unban_embed03.add_field(name="ID do usuário:", value=user.id)
             unban_embed03.add_field(name="Autor:", value=message.author.mention)
             await client.send_message(canal, embed=unban_embed03)
@@ -134,7 +134,7 @@ async def on_message(message):
         except discord.errors.Forbidden:
             ban_embed04 = discord.Embed(title="Utilize o comando: '/unban <ID do usuário>'.", color=0xFF0000)
             ban_embed04.set_footer(text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
-            return await client.send_message(message.channel, embed=ban_embed04)
+            return await client.send_message(message.channel, embed=unban_embed04)
         except discord.errors.NotFound:
             unban_embed04 = discord.Embed(title="Utilize o comando: '/unban <ID do usuário>'.", color=0xFF0000)
             unban_embed04.set_footer(text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
