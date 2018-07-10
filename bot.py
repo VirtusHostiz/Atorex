f58e64e8e608b5c1e0925882a299fa6fcf1a4083
yWilliamPDL <34868604+yWilliamPDL@users.noreply.github.com>
2018-07-09 23:14:45 -0300

    Update

--- bot.py
+++ bot.py
@@ -131,10 +131,6 @@ async def on_message(message):
             unban_embed04 = discord.Embed(title="Utilize o comando: '/unban <ID do usuário>'.", color=0xFF0000)
             unban_embed04.set_footer(text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
             return await client.send_message(message.channel, embed=unban_embed04)
-        except discord.errors.HTTPException:
-            unban_embed04 = discord.Embed(title="Utilize o comando: '/unban <ID do usuário>'.", color=0xFF0000)
-            unban_embed04.set_footer(text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
-            return await client.send_message(message.channel, embed=unban_embed04)
         finally:
             pass
 
