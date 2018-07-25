fb9b4cb4e1bbd2eec7c9c4fca206c9354cfaa0e2
yWilliamPDL <34868604+yWilliamPDL@users.noreply.github.com>
2018-07-25 12:17:16 -0300

    Update

--- BOT.py
+++ BOT.py
@@ -21,7 +21,7 @@ async def on_ready():
     print('[BOT ONLINE]')    
     while True:
         r = requests.get('https://api.mcsrvstat.us/1/play.atorexmc.com').json()
-        ip = r['ip']
+        ip = r['srv']['host']
         jogadores = r['players']['online']
         maximo = r['players']['max']
         versao = r['version']
