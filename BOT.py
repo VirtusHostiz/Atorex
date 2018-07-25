4ac75a26f8878b1bb869821b7bc3b843fe87d799
yWilliamPDL <34868604+yWilliamPDL@users.noreply.github.com>
2018-07-25 00:26:50 -0300

    Update

--- BOT.py
+++ BOT.py
@@ -28,7 +28,11 @@ async def on_ready():
         canal02 = client.get_channel('471513680614260737')
         canal03 = client.get_channel('471491546924187670')
         await client.edit_channel(channel=canal01, name="👥| Jogadores: {}/{}".format(online, maximo))
-        await client.edit_channel(channel=canal03, name="👥| Membros: {}".format(str(len(set(client.get_all_members()))))
+        await client.edit_channel(channel=canal03, name="👥| Membros: {}".format(str(len(set(client.get_all_members())))))
+        if status == "true":
+            await client.edit_channel(channel=canal02, name="🎇| Status: Online")
+        if status == "false":
+            await client.edit_channel(channel=canal02, name="🎇| Status: Offline")
         await asyncio.sleep(1)
 
 
