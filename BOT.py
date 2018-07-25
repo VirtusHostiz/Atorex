d0a2dd7de284232b0f4bd83b949e87abd3bdfd98
yWilliamPDL <34868604+yWilliamPDL@users.noreply.github.com>
2018-07-25 12:30:50 -0300

    Update

--- BOT.py
+++ BOT.py
@@ -20,7 +20,7 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name=prefix+'comandos', type=2))
     print('[BOT ONLINE]')    
     while True:
-        r = requests.get('https://api.mcsrvstat.us/1/play.atorexmc.coma').json()
+        r = requests.get('https://api.mcsrvstat.us/1/play.atorexmc.com').json()
         ip = r['hostname']
         jogadores = r['players']['online']
         maximo = r['players']['max']
