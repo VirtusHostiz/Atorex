70fb895680df5ac39ee88aca5db5a02cb79a5b5e
yWilliamPDL <34868604+yWilliamPDL@users.noreply.github.com>
2018-07-28 18:30:05 -0300

    Update

--- BOT.py
+++ BOT.py
@@ -602,10 +602,14 @@ async def on_message(message):
             manu_embed = discord.Embed(title="Você não tem permissões necessárias para utilizar este comando.", color=0xFF0000)
             manu_embed.set_footer(icon_url=message.author.avatar_url, text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
             return await client.send_message(message.channel, embed=manu_embed)
-        if manu != "on" and "off":
+        if manu != "on":
             manu_embed02 = discord.Embed(title="Utilize o comando: '/manu on' ou '/manu off'.", color=0xFF0000)
             manu_embed02.set_footer(icon_url=message.author.avatar_url, text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
             return await client.send_message(message.channel, embed=manu_embed02)
+        if manu != "off":
+            manu_embed03 = discord.Embed(title="Utilize o comando: '/manu on' ou '/manu off'.", color=0xFF0000)
+            manu_embed03.set_footer(icon_url=message.author.avatar_url, text="• Comando enviado por {}#{}.".format(message.author.name, message.author.discriminator))
+            return await client.send_message(message.channel, embed=manu_embed03)
         try:
             manu = message.content[6:]
             canal = client.get_channel('472868023473274882')
